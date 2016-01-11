de1eb0f80db4c2bee8c66e277324448f4f95156b
Tyslan <verschingellorenz@hotmail.com>
2016-01-11 09:54:34 +0100

    voting done

--- NerdWatch-iOS/ViewController.swift
+++ NerdWatch-iOS/ViewController.swift
@@ -14,9 +14,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var yearLabel: UILabel!
+    @IBOutlet weak var votesLabel: UILabel!
     
     var id:String = "5661e8e348b1443901f26b92"
     let baseUrl = "https://lorenzverschingel.herokuapp.com/api/movies/"
+    
+    var votes:Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,9 +35,18 @@ class ViewController: UIViewController {
                     self.titleLabel.text = movieJSON["title"].string
                     self.descriptionLabel.text = movieJSON["description"].string
                     self.yearLabel.text = movieJSON["year"].string
+                    self.votes = movieJSON["upvotes"].int!
+                    self.votesLabel.text = String(self.votes)
                     print(movieJSON)
                 }
         }
+    }
+    
+    @IBAction func voteUp() {
+        votes += 1
+        let url = baseUrl + id + "/upvote"
+        Alamofire.request(.PUT, url)
+        votesLabel.text = String(self.votes)
     }
 
     override func didReceiveMemoryWarning() {
